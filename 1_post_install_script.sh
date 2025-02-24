f6fdb48900912dbd1aed0cdd5eb06dcc34ef641e
Micha Cassola <mich@casso.la>
2025-02-24 17:12:02 +0100

    Update 1_post_install_script.sh

--- 1_post_install_script.sh
+++ 1_post_install_script.sh
@@ -14,21 +14,23 @@ if ! command -v envsubst >/dev/null && [ ! -f "$(command -v envsubst)" ]; then
     unyp system-install gettext
 fi
 
+sed -e '/\[Install\]/a\' -e 'Alias=crowdsec.service cs.service' -i config/crowdsec.service
+sed -e "s|/etc/systemd/system/crowdsec.service|/etc/systemd/system/uny-crowdsec.service|" -i ./wizard.sh
 ./wizard.sh -i --unattended
 
-if [ ! -f /etc/systemd/system/uny-crowdsec.service ]; then
-    if systemctl is-active crowdsec -q; then
-        systemctl stop crowdsec
-        if systemctl is-enabled crowdsec -q; then
-            systemctl disable crowdsec
-        fi
-    fi
-fi
+#if [ ! -f /etc/systemd/system/uny-crowdsec.service ]; then
+#    if systemctl is-active crowdsec -q; then
+#        systemctl stop crowdsec
+#        if systemctl is-enabled crowdsec -q; then
+#            systemctl disable crowdsec
+#        fi
+#    fi
+#fi
 
-cp -f config/crowdsec.service /etc/systemd/system/uny-crowdsec.service
-sed "s|.*Alias=.*||g" -i /etc/systemd/system/uny-crowdsec.service
-sed -e '/\[Install\]/a\' -e 'Alias=crowdsec.service cs.service' -i /etc/systemd/system/uny-crowdsec.service
-systemctl daemon-reload
+#cp -f config/crowdsec.service /etc/systemd/system/uny-crowdsec.service
+#sed "s|.*Alias=.*||g" -i /etc/systemd/system/uny-crowdsec.service
+#sed -e '/\[Install\]/a\' -e 'Alias=crowdsec.service cs.service' -i /etc/systemd/system/uny-crowdsec.service
+#systemctl daemon-reload
 
 #############################################################################################
 ### End of script
